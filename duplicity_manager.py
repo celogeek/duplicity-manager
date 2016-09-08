e1621ddcde2d78c87770b4237955024f39fe6b97
celogeek <me@celogeek.com>
2016-09-08 20:21:14 +0200

    add restore

--- duplicity_manager.py
+++ duplicity_manager.py
@@ -17,10 +17,13 @@ def main():
 def process(config, program, action=None, src=None, dest=None):
     if (action == None):
         for k in [
-            "list                - list all possibile backups",
-            "backup [SRC]        - backup the SRC",
-            "backup-script [SRC] - display the backup script",
-            "backup-all          - backup everything"
+            "list                        - list all possibile backups",
+            "backup [SRC]                - backup the SRC",
+            "backup-script [SRC]         - display the backup script",
+            "backup-all [server]         - backup everything",
+            "restore [SRC] [DEST]        - restore the src",
+            "restore-script [SRC] [DEST] - restore the src",
+            "restore-force [SRC] [DEST]  - restore with force the src"
         ]:
             print sys.argv[0] + " " + k
         sys.exit(0)
@@ -37,7 +40,18 @@ def process(config, program, action=None, src=None, dest=None):
         os.remove(script_name)
     elif(action == "backup-all"):
         for k in config.get("Actions", {}).keys():
-            process(config, program, "backup", k)
+            if config["Actions"][k]["server"] == src:
+                process(config, program, "backup", k)
+    elif(action == "restore"):
+        script_name = generate_restore(config, src, dest)
+        os.system(script_name)
+    elif(action == "restore-script"):
+        script_name = generate_restore(config, src, dest)
+        os.system("cat \"" + script_name+"\"")
+        os.remove(script_name)
+    elif(action == "restore-force"):
+        script_name = generate_restore(config, src, dest, force=True)
+        os.system(script_name)
 
 def generate(config, action):
     if (action == None):
@@ -86,6 +100,49 @@ def generate(config, action):
             )
         )
     )
+    # cleanup script
+    content.append("rm $0")
+
+    script = tempfile.NamedTemporaryFile(delete=False)
+    script.write("\n".join(content))
+    script.write("\n")
+    script.close()
+
+    os.chmod(script.name, 0700)
+    return script.name
+
+def generate_restore(config, action, dest, force=False):
+    if (action == None):
+        print("Missing action")
+        sys.exit(1)
+
+    if (dest == None):
+        print("Missing destination")
+        sys.exit(1)
+
+    globalParams = config.get("Global", [])
+    actionParams = config.get("Actions", {}).get(action)
+    if (actionParams == None):
+        print("Action \"" + action + "\" doesn't exists !")
+        sys.exit(1)
+    serverParams = config.get("Servers", {}).get(actionParams.get("server", ""), {})
+
+    def get_all(key, defaults=None):
+        return globalParams.get(key, defaults) + serverParams.get(key, defaults) + actionParams.get(key, defaults)
+
+    content = ["#!/usr/bin/env bash", "set -ex"]
+    # export envs
+    content.extend(["export " + k for k in get_all("envs", [])])
+    # setup minimum limit
+    content.append("ulimit -n 1024")
+    # create duplicity commands
+    content.append("duplicity \\")
+    content.append("    --verbosity=info \\")
+    content.append("    --num-retries=3 \\")
+    if force:
+        content.append("    --force \\")
+    content.append("    \"" + serverParams.get("base", "") + actionParams.get("to", "") + "\" \\")
+    content.append("    \"" + dest + "\"")
     # cleanup script
     content.append("rm $0")
 
